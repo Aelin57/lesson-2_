c27e32fc8837249e1f99bc28563d4f1eecf500bf
Aelin57 <775rinatyzy@gmail.com>
2020-12-18 21:32:32 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Main {
+    public static void print() {
+        System.out.println("Hello world!");
+
+    }
+
+    public static void printOurWord(String word) {
+        System.out.println("Ваше слово :" + word);
+        word = "Hello";
+
+    }
+}
